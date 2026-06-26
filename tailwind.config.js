ae326d51910e94bfc9e4832006ae298071fa2662
Mariana <marianaemiliasanchez24@gmail.com>
2026-06-26 11:24:59 -0400

    Fix: logo and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,37 +14,11 @@ module.exports = {
     'bg-green-500',
     'bg-red-500',
     'text-white'
-    
+
   ],
   theme: {
     extend: {
-      keyframes: {
-        'slide-left': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        },
-        typing: {
-          "0%": {
-            width: "0%",
-            visibility: "hidden"
-          },
-          "100%": {
-            width: "100%"
-          }
-        },
-        blink: {
-          "50%": {
-            borderColor: "transparent"
-          },
-          "100%": {
-            borderColor: "white"
-          }
-        }
-      },
-      animation: {
-        'slide-left-infinite': 'slide-left 6s linear infinite',
-        typing: "typing 2.5s steps(20) infinite alternate, blink .7s infinite"
-      },
+      
       container: {
         center: true,
         padding: {
